a7a302aa5c8cb1b606099916bd728d1b2524a6c7
Nikolay Pianikov <nikolayp@mail.ru>
2024-06-06 13:20:19 +0300

    Improved Dockerfile

--- build/Program.cs
+++ build/Program.cs
@@ -1,7 +1,7 @@
 ﻿const string image = "dotnetsdk:latest";
 const string repoImage = "nikolayp/dotnetsdk:latest";
 
-new DockerCustom("build", "-t", image, ".").TryRun();
+new DockerCustom("build", "-t", image, "docker").TryRun();
 new DockerCustom("login").TryRun();
 new DockerCustom("tag", image, repoImage).TryRun();
 new DockerCustom("image", "push", repoImage).TryRun();
